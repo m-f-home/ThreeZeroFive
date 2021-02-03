56696949ba33a8e88d3a08ec0838186b393367b3
fangzheng <fangzheng@henhaoji.com>
2021-02-03 17:32:32 +0800

    文件更新

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
+  <common-header />
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'app',
+  setup() {
 
-#nav {
-  padding: 30px;
+  }
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  #app {
+    width: 100%;
+    height: 100%;
+  }
 </style>
---
+++ src/components/common_header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="common-header">
+    <div class="user-info">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'common-header',
+  props: {},
+  setup(props) {
+    // console.log(props)
+  }
+}
+</script>
+
+<style lang="scss">
+  .common-header {
+    width: 400px;
+    height: 100%;
+  }
+</style>
